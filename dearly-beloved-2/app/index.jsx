eb53b30aa6a17ebe30b3fb6b32b00a9b44daf1f8
KianDoyle <kianrk@icloud.com>
2023-12-29 23:18:48 +0000

    dropbox full integration

--- dearly-beloved-2/app/index.jsx
+++ dearly-beloved-2/app/index.jsx
@@ -1,85 +1,142 @@
-import { access } from 'fs';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import querystring from 'querystring';
 import styles from '../app/styles.module.css';
 
-const { Dropbox } = require('dropbox');
+const clientId = 'woixl1iflym0w59';
+const clientSecret = 'zwm6elhd2nrlfkg'; // Replace with your app's client secret
 
-const dbox = new Dropbox({
-    clientId: w5t0j6pgtnuh65m,
-    clientSecret: swxie9u8h8asfh8,
-});
-
-// const authoriseURL = dbox.getAuthenticationUrl('http')
-
-const accessToken = "sl.Bo9w_Mgf0__pJUmZE6OPn_kByIynB-j6CYrX-XidCqy74HqvDxZxCbhUX5-2f7HMD0ykCcPJ420D7ZiYOLExHew-NKKI6dVjLD3IU8Dvt3ErV3TzG9pbG9pFAWO-QUncAZ8cdFVoaFtz";
-
-dbox.setAccessToken(accessToken);
-{/* add link to dropbox here */}
-
-// Almost done - need to make machine in Lez's dropbox and input the details here 
-
-async function listFilesInFolder(folderPath) {
+async function Index() {
     try {
-        const response = await dbox.listFilesInFolder({
-            path: folderPath,
-        });
+        const [images, setImages] = useState([]);
 
-        const imageFiles = response.entries.filter((entry) => 
-            entry.name.match(/\.(jpg|jpeg|png|gif)$/i)
-        );
-        
-        return imageFiles;
-    } catch(error) {
-        console.error(error);
-        throw error;
-    }
-}
+        useEffect(() => {
+            const initiateAuth = async () => {
+                const queryParams = querystring.stringify({
+                    response_type: 'code',
+                    client_id: clientId,
+                    redirect_uri: `${window.location.origin}${window.location.pathname}`, // Current page's path
+                });
 
+                // Redirect to Dropbox for authentication
+                window.location.href = `https://www.dropbox.com/oauth2/authorize?${queryParams}`;
+            };
 
-function Index() {
-    return (
-        <div className={styles.index-portfolio-images}>
-            {/* import images from drop box link and 'style' them */}
-            <div className={styles.index-row}>
-                <div className={styles.index-column}>
-                    {imageFiles.slice(0,4).map((image) => (
-                        <img
-                            key={image.client_modified}
-                            src={image.path_display}
-                            alt={image.name}
-                      />
-                    ))};
-                </div>
-                <div className={styles.index-column}>
-                    {imageFiles.slice(5,9).map((image) => (
-                        <img
-                            key={image.client_modified}
-                            src={image.path_display}
-                            alt={image.name}
-                      />
-                    ))};
-                </div>
-                <div className={styles.index-column}>
-                    {imageFiles.slice(10,14).map((image) => (
-                        <img
-                            key={image.client_modified}
-                            src={image.path_display}
-                            alt={image.name}
-                      />
-                    ))};
-                </div>
-                <div className={styles.index-column}>
-                    {imageFiles.slice(15,19).map((image) => (
-                        <img
-                            key={image.client_modified}
-                            src={image.path_display}
-                            alt={image.name}
-                      />
-                    ))};
+            const processOAuthFlow = async () => {
+                // Check if the URL contains the code parameter
+                const urlParams = new URLSearchParams(window.location.search);
+                const code = urlParams.get('code');
+
+                if (code) {
+                    // Exchange code for access token
+                    const tokenUrl = 'https://api.dropboxapi.com/oauth2/token'; // Dropbox token endpoint
+                    const redirectUri = `${window.location.origin}${window.location.pathname}`;
+
+                    try {
+                        const response = await axios.post(tokenUrl, {
+                            code,
+                            grant_type: 'authorization_code',
+                            client_id: clientId,
+                            client_secret: clientSecret,
+                            redirect_uri: redirectUri,
+                        });
+
+                        const accessToken = response.data.access_token;
+
+                        // Fetch images with the obtained access token
+                        const fetchedImages = await fetchImagesFromDropbox(accessToken);
+                        setImages(fetchedImages);
+                    } catch (error) {
+                        console.error('Error fetching access token:', error);
+                    }
+                } else {
+                    // If no code parameter, initiate authentication
+                    initiateAuth();
+                }
+            };
+
+            processOAuthFlow();
+
+        }, []);
+
+        const fetchImagesFromDropbox = async (accessToken) => {
+            try {
+                const response = await axios.post(
+                    'https://api.dropboxapi.com/2/files/list_folder',
+                    {
+                        path: '/Website', // Replace with your Dropbox folder path
+                        recursive: false,
+                        include_media_info: true,
+                        include_deleted: false,
+                        include_has_explicit_shared_members: false,
+                        include_mounted_folders: true,
+                    },
+                    {
+                        headers: {
+                            Authorization: `Bearer ${accessToken}`,
+                            'Content-Type': 'application/json',
+                        },
+                    }
+                );
+
+                const imageFiles = response.data.entries;
+            } catch (error) {
+                console.error('Error fetching images from Dropbox:', error);
+            }
+        };
+
+        return (
+            <div className={styles.index - portfolio - images}>
+                {/* import images from drop box link and 'style' them */}
+                <div className={styles.index - row}>
+                    <div className={styles.index - column}>
+                        {imageFiles.slice(0, 5).map((image) => (
+                            <img
+                                key={image.client_modified}
+                                src={image.path_display}
+                                alt={image.name}
+                            />
+                        ))};
+                    </div>
+                    <div className={styles.index - column}>
+                        {imageFiles.slice(5, 10).map((image) => (
+                            <img
+                                key={image.client_modified}
+                                src={image.path_display}
+                                alt={image.name}
+                            />
+                        ))};
+                    </div>
+                    <div className={styles.index - column}>
+                        {imageFiles.slice(10, 15).map((image) => (
+                            <img
+                                key={image.client_modified}
+                                src={image.path_display}
+                                alt={image.name}
+                            />
+                        ))};
+                    </div>
+                    <div className={styles.index - column}>
+                        {imageFiles.slice(15, 20).map((image) => (
+                            <img
+                                key={image.client_modified}
+                                src={image.path_display}
+                                alt={image.name}
+                            />
+                        ))};
+                    </div>
                 </div>
             </div>
-        </div>
-    );
-}
+        );
+
+    } catch (error) {
+        return (
+            <div>
+                there was an error returning images.
+            </div>
+        );
+    }
+
+};
 
 export default Index;
