ebb504c50568905b92d68d8e62f12fc8c0d65027
KianDoyle <kianrk@icloud.com>
2023-12-30 18:58:59 +0000

    directory error

--- dearly-beloved-2/pages/_app.tsx
+++ dearly-beloved-2/pages/_app.tsx
@@ -1,5 +1,5 @@
 import { AppProps } from 'next/app';
-import '../styles/global.css';
+import '../globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   // Additional setup or context providers can go here
