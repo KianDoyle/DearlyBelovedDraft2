4c227ae154d3059db5bcb47999c9dee0d0f1b5e4
KianDoyle <kianrk@icloud.com>
2024-02-13 02:52:42 +0000

    file name changes

--- dearly-beloved-2/pages/home.jsx
+++ dearly-beloved-2/pages/home.jsx
@@ -26,10 +26,10 @@ const Home = () => {
                 {images.map((image) => (
                     <div className={styles.indexcolumn} key={image.id} >
                         <Image
-                            src={`/uploads/${image.path_display}`}
+                            src={`"/uploads/${image.path_display}"`}
                             alt={image.name}
                             className='object-contain'
-                            objectFit='cover'
+                            layout='cover'
                             width={image.width}
                             height={image.height}
                         />
